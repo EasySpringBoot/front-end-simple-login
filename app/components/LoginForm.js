612f627c6e174581d3f41ea6d017399be8b2f18a
之剑 <guangjian.cgj@alibaba-inc.com>
2018-11-10 22:47:30 +0800

    import $ from 'jquery'

--- app/components/LoginForm.js
+++ app/components/LoginForm.js
@@ -7,6 +7,8 @@ import Button from '@material-ui/core/Button';
 import FormControl from '@material-ui/core/FormControl';
 import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
+import FormHelperText from "@material-ui/core/es/FormHelperText/FormHelperText";
+import $ from 'jquery'
 
 const styles = theme => ({
     container: {
@@ -34,6 +36,54 @@ const styles = theme => ({
 
 class LoginForm extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            helperText: ''
+        };
+
+        // 这边绑定是必要的，这样 `this` (代表 LoginForm) 才能在回调函数中使用，例如：this.setState
+        this.handleClick = this.handleClick.bind(this);
+        this.handleUsernameChange = this.handleUsernameChange.bind(this);
+    }
+
+    handleClick(event) {
+        console.log(event.currentTarget);
+        const username = document.getElementById('username').value
+        const password = document.getElementById('password').value
+        const data = {
+            username: username,
+            password: password
+        };
+
+        console.log(data);
+
+        $.ajax({
+            url: '/login.json',
+            data: data,
+            type: 'POST',
+            success: (res) => {
+                console.log(data)
+            },
+            error: (err) => {
+                console.log(err)
+            }
+        });
+    };
+
+    handleUsernameChange(event) {
+        console.log(event.currentTarget);
+        if (event.currentTarget.value.length < 3) {
+            this.setState({
+                helperText: '用户名长度不得小于3'
+            })
+        } else {
+            this.setState({
+                helperText: ''
+            })
+        }
+    };
+
     render() {
         const {classes} = this.props;
 
@@ -44,7 +94,8 @@ class LoginForm extends React.Component {
 
                         <FormControl className={classes.formControl}>
                             <InputLabel>用户名</InputLabel>
-                            <Input id="username"/>
+                            <Input id="username" onChange={this.handleUsernameChange} autoFocus={true}/>
+                            <FormHelperText id="component-helper-text">{this.state.helperText}</FormHelperText>
                         </FormControl>
 
                         <FormControl className={classes.formControl}>
@@ -54,6 +105,7 @@ class LoginForm extends React.Component {
 
                         <div className={classes.inlineButton}>
                             <Button
+                                onClick={this.handleClick}
                                 variant="contained"
                                 color="primary"
                                 className={classes.button}>
